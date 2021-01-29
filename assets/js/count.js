0cfef848c66d7a631d11b0a1c02316ee8ce80304
Shivam Chauhan <shivam25.srgc@gmail.com>
2021-01-30 03:04:51 +0530

    initial commit

---
+++ assets/js/count.js
@@ -0,0 +1,8 @@
+var uid='dhull442';var db='1';var delta_time='19800';var new_session='yes';var user_status='regular';var uniqueID='XPV2T38AAAEAAGGdEMQAAAAG';var sessionID='XPV2T38AAAEAAGGdEMQAAAAG';if('regular'=='premium'&&'no'!='yes'){document.getElementById("wtstimer1798973").innerHTML=db+'::'+uid+'::'+delta_time+'::'+new_session+'::'+uniqueID+'::'+sessionID;}
+if('yes'!='yes'){if('en'=='fr'){var host='https://www.web-stat.fr';}
+else{var host='https://www.web-stat.com';}
+var idTag='PublicID=dhull442';if('stats_menu'=='visitor_details'){var click_target='live_visitors.htm';}
+else if('stats_menu'=='all_in_one'){var click_target='all_in_one.htm';}
+else{var click_target='checkstats.htm';}
+var prefix='https://wts.one';if('stats_menu'=='no_click'){document.getElementById("wts1798973").innerHTML='<span style="display:inline-block; text-align:center;"><img src="'+prefix+'/draw_counter.htm?314::::::::'+'::'+Math.random()+'" width="0" height="" border="0" style="; padding:0px ! important; margin:0px auto ! important; box-sizing: content-box ! important;"></span>';}
+else{document.getElementById("wts1798973").innerHTML='<span style="display:inline-block; text-align:center;"><a href="'+host+'/stats/'+click_target+'?'+idTag+'" target="new" border="0"><img src="'+prefix+'/draw_counter.htm?314::::::::'+'::'+Math.random()+'" width="0" height="" border="0" style="; padding:0px ! important; margin:0px auto ! important;  box-sizing: content-box ! important;"></a></span>';}}
